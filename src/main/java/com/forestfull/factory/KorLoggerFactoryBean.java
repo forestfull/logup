d4f68e5a9618a1d3e084c60e30667160bd2b78d4
hs.ko <tpsxja)!(@>
2025-01-22 15:36:03 +0900

    주석

--- src/main/java/com/forestfull/factory/KorLoggerFactoryBean.java
+++ src/main/java/com/forestfull/factory/KorLoggerFactoryBean.java
@@ -4,6 +4,23 @@ import lombok.*;
 
 import java.text.SimpleDateFormat;
 
+/**
+ * korLogger's Configuration for spring project
+ * @default-value
+ * <div>
+ *      <h3>log file path</h3>
+ *      <p>logFileDirectory - "classpath:logs/"</p>
+ * </div>
+ * <hr>
+ * <div>
+ *      <h3>{@link Formatter}</h3>
+ *      <p>placeHolder - "{datetime}{thread}:{level} - {msg}{new-line}"</p>
+ *      <p>datetime - new {@link SimpleDateFormat}("yyyy-MM-dd HH:mm:ss")</p>
+ *      <p>{@link Level} - {@link Level}.INFO</p>
+ * </div>
+ * @version JDK 1.6
+ * @author Hyeonseok Ko
+ */
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
