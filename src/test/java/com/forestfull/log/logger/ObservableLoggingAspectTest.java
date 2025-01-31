5a761e751772a8b3aec4d3d5052455fe0a09f1ef
hs.ko <tpsxja)!(@>
2025-01-31 10:20:14 +0900

    TODO LIST  - aop 체크  - JDBC 스캐닝

--- src/test/java/com/forestfull/log/logger/ObservableLoggingAspectTest.java
+++ src/test/java/com/forestfull/log/logger/ObservableLoggingAspectTest.java
@@ -2,23 +2,22 @@ package com.forestfull.log.logger;
 
 import com.forestfull.log.logger.util.Log;
 import org.junit.jupiter.api.Test;
+
 class ObservableLoggingAspectTest {
 
-    public static class Con{
-        public void testMethod(Object o, Object o1){
-            Log.info(o, o1);
-        }
+	public void testMethod(Object o, Object o1) {
+		Log.info(o, o1);
+	}
 
-        public void test(Object o){
-            testMethod(o, null);
-        }
-    }
+	public void test(Object o) {
+		testMethod(o, null);
+	}
 
-    @Test
-    void test() {
-        Con constructor = new Con();
-        constructor.testMethod("1", 1);
-        constructor.testMethod("2", 2);
-        constructor.test("2");
-    }
+	@Test
+	void test() {
+		ObservableLoggingAspectTest constructor = new ObservableLoggingAspectTest();
+		constructor.testMethod("1", 1);
+		constructor.testMethod("2", 2);
+		constructor.test("2");
+	}
 }
