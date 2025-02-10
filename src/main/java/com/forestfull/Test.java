e75d80607db3a4a32634af0466194c0e19bb2eee
hs.ko <tpsxja)!(@>
2025-02-10 13:52:26 +0900

    프로세서가 컴파일 전 먼저 실행되어 에러가 발생 플러그인으로 대체하고 해당 프로세서 파일 제거

--- src/main/java/com/forestfull/Test.java
+++ src/main/java/com/forestfull/Test.java
@@ -1,11 +1,7 @@
 package com.forestfull;
 
-import com.forestfull.logger.Level;
-import com.forestfull.logger.annotation.ObservableArguments;
-import com.forestfull.logger.util.FileRecorder;
-import com.forestfull.logger.util.KoLoggerFactoryBean;
+import com.forestfull.lombok.ObservableArguments;
 import com.forestfull.logger.util.Log;
-import com.forestfull.logger.util.LogFormatter;
 
 public class Test {
 
@@ -16,23 +12,9 @@ public class Test {
 
     public static void main(String[] args) {
 
-        KoLoggerFactoryBean.builder()
-                .level(Level.ERROR)
-                .logFormatter(LogFormatter.builder().build())
-                .fileRecorder(FileRecorder.builder().logFileDirectory("logs/").build())
-                .start();
-
         new Test().test();
 
         Log.error("Hello World");
-
-
-        KoLoggerFactoryBean.builder()
-                .level(Level.INFO)
-                .logFormatter(LogFormatter.builder().build())
-                .fileRecorder(FileRecorder.builder().logFileDirectory("logs/").build())
-                .start();
-
         Log.info("Hello World");
     }
 }
