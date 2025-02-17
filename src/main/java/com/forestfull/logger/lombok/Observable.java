b3d19284deb6e7f637867cbb8d60e535fdd58983
Xasquatch <bigfoot7774@gmail.com>
2025-02-17 12:58:04 +0900

    1.1.0 안정화 버전

--- src/main/java/com/forestfull/logger/lombok/Observable.java
+++ src/main/java/com/forestfull/logger/lombok/Observable.java
@@ -1,13 +1,11 @@
 package com.forestfull.logger.lombok;
 
-
 import com.forestfull.logger.Level;
-
 import java.lang.annotation.*;
 
 @Documented
 @Target({ElementType.METHOD, ElementType.LOCAL_VARIABLE})
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Observable {
+@Retention(RetentionPolicy.SOURCE)
+@interface Observable {
     Level level() default Level.ALL;
 }
