713d271377162f4c06d2ebde7470b13af4f6bd69
Xasquatch <bigfoot7774@gmail.com>
2025-02-17 22:07:36 +0900

    빌더 재적용

--- src/main/java/com/forestfull/logger/util/FileRecorder.java
+++ src/main/java/com/forestfull/logger/util/FileRecorder.java
@@ -1,131 +1,44 @@
 package com.forestfull.logger.util;
 
+import lombok.Builder;
+import lombok.Getter;
+
 import java.text.SimpleDateFormat;
 
+@Getter
+@Builder
 public class FileRecorder {
-	private String placeholder;
-	private SimpleDateFormat dateFormat;
-	private String logFileDirectory;
+    @Builder.Default
+    private String placeholder = getDefaultPlaceHolder();
+    @Builder.Default
+    private SimpleDateFormat dateFormat = getDefaultDateFormat();
+    @Builder.Default
+    private String logFileDirectory = "logs/";
 
-	void setPlaceholder(String placeholder) {
-		this.placeholder = placeholder;
-	}
+    void setPlaceholder(String placeholder) {
+        this.placeholder = placeholder;
+    }
 
-	void setDateFormat(SimpleDateFormat dateFormat) {
-		this.dateFormat = dateFormat;
-	}
+    void setDateFormat(SimpleDateFormat dateFormat) {
+        this.dateFormat = dateFormat;
+    }
 
-	void setLogFileDirectory(String logFileDirectory) {
-		this.logFileDirectory = logFileDirectory;
-	}
+    void setLogFileDirectory(String logFileDirectory) {
+        this.logFileDirectory = logFileDirectory;
+    }
 
-	static String getDefaultPlaceHolder() {
-		return FileRecorder.FilePattern.PROJECT_NAME + "{date}.log";
-	}
+    static String getDefaultPlaceHolder() {
+        return FileRecorder.FilePattern.PROJECT_NAME + "{date}.log";
+    }
 
-	static SimpleDateFormat getDefaultDateFormat() {
-		return new SimpleDateFormat("yyyy-MM-dd");
-	}
+    static SimpleDateFormat getDefaultDateFormat() {
+        return new SimpleDateFormat("yyyy-MM-dd");
+    }
 
-	private static String $default$placeholder() {
-		return getDefaultPlaceHolder();
-	}
-
-	private static SimpleDateFormat $default$dateFormat() {
-		return getDefaultDateFormat();
-	}
-
-	private static String $default$logFileDirectory() {
-		return "logs/";
-	}
-
-	FileRecorder(String placeholder, SimpleDateFormat dateFormat, String logFileDirectory) {
-		this.placeholder = placeholder;
-		this.dateFormat = dateFormat;
-		this.logFileDirectory = logFileDirectory;
-	}
-
-	public static FileRecorderBuilder builder() {
-		return new FileRecorderBuilder();
-	}
-
-	public String getPlaceholder() {
-		return this.placeholder;
-	}
-
-	public SimpleDateFormat getDateFormat() {
-		return this.dateFormat;
-	}
-
-	public String getLogFileDirectory() {
-		return this.logFileDirectory;
-	}
-
-	public static class FileRecorderBuilder {
-		private boolean placeholder$set;
-		private String placeholder;
-		private boolean dateFormat$set;
-		private SimpleDateFormat dateFormat;
-		private boolean logFileDirectory$set;
-		private String logFileDirectory;
-
-		FileRecorderBuilder() {
-		}
-
-		public FileRecorderBuilder placeholder(String placeholder) {
-			this.placeholder = placeholder;
-			this.placeholder$set = true;
-			return this;
-		}
-
-		public FileRecorderBuilder dateFormat(SimpleDateFormat dateFormat) {
-			this.dateFormat = dateFormat;
-			this.dateFormat$set = true;
-			return this;
-		}
-
-		public FileRecorderBuilder logFileDirectory(String logFileDirectory) {
-			this.logFileDirectory = logFileDirectory;
-			this.logFileDirectory$set = true;
-			return this;
-		}
-
-		public FileRecorder build() {
-			String placeholder = this.placeholder;
-			if (!this.placeholder$set) {
-				placeholder = FileRecorder.$default$placeholder();
-			}
-
-			SimpleDateFormat dateFormat = this.dateFormat;
-			if (!this.dateFormat$set) {
-				dateFormat = FileRecorder.$default$dateFormat();
-			}
-
-			String logFileDirectory = this.logFileDirectory;
-			if (!this.logFileDirectory$set) {
-				logFileDirectory = FileRecorder.$default$logFileDirectory();
-			}
-
-			return new FileRecorder(placeholder, dateFormat, logFileDirectory);
-		}
-
-		public String toString() {
-			return "FileRecorder.FileRecorderBuilder(placeholder=" + this.placeholder + ", dateFormat=" + this.dateFormat + ", logFileDirectory=" + this.logFileDirectory + ")";
-		}
-	}
-
-	public static class FilePattern {
-		public static final String[] filePath = System.getProperty("user.dir").split("[/|\\\\]");
-		public static final String PROJECT_NAME;
-		public static final String DATE = "{date}";
-		public static final String DEFAULT;
-
-		public FilePattern() {
-		}
-
-		static {
-			PROJECT_NAME = filePath[filePath.length - 1];
-			DEFAULT = PROJECT_NAME + "{date}" + ".log";
-		}
-	}
+    public static class FilePattern {
+        public static final String[] filePath = System.getProperty("user.dir").split("[/|\\\\]");
+        public static final String PROJECT_NAME = filePath[filePath.length - 1];
+        public static final String DATE = "{date}";
+        public static final String DEFAULT = PROJECT_NAME + "{date}" + ".log";
+    }
 }
