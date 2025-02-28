b7e9d3016399d18c2830d030fd7acf19eb20cee8
hs.ko <tpsxja)!(@>
2025-02-28 14:59:15 +0900

    이니셜라이저에서 인식할 수 있도록 추가구현

--- src/main/java/com/forestfull/log/up/spring/SpringInitializer.java
+++ src/main/java/com/forestfull/log/up/spring/SpringInitializer.java
@@ -1,11 +1,28 @@
 package com.forestfull.log.up.spring;
 
+import com.forestfull.log.up.util.LogUpConfigLoader;
+import com.forestfull.log.up.util.LogUpFactoryBean;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
-public class SpringInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+public class SpringInitializer extends LogUpFactoryBean implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
         applicationContext.getBeanFactory().registerSingleton("observableConfig", new ObservableConfig());
+
+        try {
+            final LogUpProperties logUpProperties = applicationContext.getBean(LogUpProperties.class);
+            configureProperties(logUpProperties.toProperties());
+            System.out.println("Successfully configured LogUpProperties.");
+        } catch (Exception e) {
+            System.out.println("Starting default configuration: LogUpProperties not found.");
+            configureProperties();
+        } finally {
+            if (LogUpFactoryBean.level == null)
+                LogUpConfigLoader.loggingInitializeManual();
+
+            defaultInitialize();
+        }
     }
 }
