ea0a594cd17447d3fd1b63fface8717b9e8665ce
Xasquatch <bigfoot7774@gmail.com>
2025-03-03 00:16:35 +0900

    프로파일별로 구분할 수 있도록 기능 추가 완료

--- src/main/java/com/forestfull/log/up/util/LogUpConfigLoader.java
+++ src/main/java/com/forestfull/log/up/util/LogUpConfigLoader.java
@@ -29,12 +29,19 @@ public class LogUpConfigLoader {
             "application.yml"
     };
 
+    /**
+     * Loads the LogUpProperties configuration by initializing the Spring environment
+     * and binding the properties from the configuration files.
+     *
+     * @return LogUpProperties object with the loaded configuration or null if binding fails
+     * @author <a href="https://vigfoot.com">Vigfoot</a>
+     */
     public static LogUpProperties loadConfig() {
         // Spring 환경 설정 초기화
         StandardEnvironment environment = new StandardEnvironment();
         MutablePropertySources propertySources = environment.getPropertySources();
 
-        String activeProfile = environment.getActiveProfiles()[0];
+        String activeProfile = environment.getActiveProfiles().length == 0 ? null : environment.getActiveProfiles()[0];
         activeProfile = activeProfile != null ? activeProfile.trim() : null;
 
         // 설정 파일 로드
@@ -43,13 +50,22 @@ public class LogUpConfigLoader {
         return Binder.get(environment).bind("logup", Bindable.of(LogUpProperties.class)).orElse(null);
     }
 
+    /**
+     * Injects property sources into the MutablePropertySources based on the active profile.
+     *
+     * @param activeProfile    the currently active profile, if any
+     * @param propertySources  the MutablePropertySources to inject the property sources into
+     * @author <a href="https://vigfoot.com">Vigfoot</a>
+     */
     private static void injectPropertySources(String activeProfile, MutablePropertySources propertySources) {
-        final String[] locations = Arrays.stream(LOCATIONS).flatMap(loc -> {
-            if (StringUtils.hasText(activeProfile))
-                return Stream.of(loc.replace("application.", "application-" + activeProfile.toLowerCase() + "."), loc);
+        String[] locations;
 
-            return Stream.of(loc);
-        }).toArray(String[]::new);
+        if (StringUtils.hasText(activeProfile)) {
+            String[] customProps = Arrays.stream(LOCATIONS).map(loc -> loc.replace("application.", "application-" + activeProfile.toLowerCase() + ".")).toArray(String[]::new);
+            locations = Stream.of(customProps, LOCATIONS).flatMap(Stream::of).toArray(String[]::new);
+        } else {
+            locations = LOCATIONS;
+        }
 
         for (String location : locations) {
             PropertySource<?> source = getPropertySources(activeProfile, location);
@@ -63,6 +79,14 @@ public class LogUpConfigLoader {
         }
     }
 
+    /**
+     * Loads a PropertySource from the specified location.
+     *
+     * @param activeProfile the currently active profile, if any
+     * @param location      the location of the property source to load
+     * @return the loaded PropertySource, or null if the source does not exist
+     * @author <a href="https://vigfoot.com">Vigfoot</a>
+     */
     private static PropertySource<?> getPropertySources(String activeProfile, String location) {
         final Resource resource = new ClassPathResource(location);
         PropertySource<?> propertySource = null;
@@ -76,15 +100,17 @@ public class LogUpConfigLoader {
 
                         final String activeProfileUnderVersion = String.valueOf(source.getProperty("spring.profiles"));
                         final String activeProfileCurrentVersion = String.valueOf(source.getProperty("spring.config.activate.on-profile"));
-                        if (activeProfileUnderVersion == null && activeProfileCurrentVersion == null) {
-                            propertySource = source;
-
-                        } else if (activeProfile != null && (activeProfile.equalsIgnoreCase(activeProfileUnderVersion) || activeProfile.equalsIgnoreCase(activeProfileCurrentVersion))) {
+                        if (activeProfile != null && (activeProfile.equalsIgnoreCase(activeProfileUnderVersion) || activeProfile.equalsIgnoreCase(activeProfileCurrentVersion))) {
                             propertySource = source;
                             return propertySource;
 
+                        } else if (propertySource == null) {
+                            propertySource = source;
+
                         }
                     }
+
+                    return propertySource;
 
                 } else {
                     propertySource = new ResourcePropertySource(location, resource);
@@ -96,7 +122,7 @@ public class LogUpConfigLoader {
     }
 
 
-    public static void loggingInitializeManual() {
+    static void loggingInitializeManual() {
         Log.LogFactory.console(System.lineSeparator() + "=================================================================================================================================================================" + System.lineSeparator());
         Log.LogFactory.console("LogUp Setting Example");
         Log.LogFactory.console(System.lineSeparator() + "=================================================================================================================================================================" + System.lineSeparator());
@@ -125,6 +151,5 @@ public class LogUpConfigLoader {
         Log.LogFactory.console("                    .logFormatter(LogFormatter.builder().build())" + System.lineSeparator());
         Log.LogFactory.console("                    .fileRecorder(FileRecorder.builder().logFileDirectory(\"logs/\").build())" + System.lineSeparator());
         Log.LogFactory.console("                    .build();" + System.lineSeparator() + System.lineSeparator());
-        Log.LogFactory.console("=================================================================================================================================================================" + System.lineSeparator() + System.lineSeparator());
     }
 }
