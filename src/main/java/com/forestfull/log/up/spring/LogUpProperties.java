51eb9ffa943b6381c8b2d7b4fc69c1f9af677f17
Xasquatch <bigfoot7774@gmail.com>
2025-03-02 21:23:31 +0900

    테스트 위해 수정

--- src/main/java/com/forestfull/log/up/spring/LogUpProperties.java
+++ src/main/java/com/forestfull/log/up/spring/LogUpProperties.java
@@ -3,14 +3,14 @@ package com.forestfull.log.up.spring;
 import com.forestfull.log.up.Level;
 import com.forestfull.log.up.util.FileRecorder;
 import com.forestfull.log.up.util.LogFormatter;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Setter
-@Getter
+@Data
 @Component
 @ConfigurationProperties(prefix = "logup")
 public class LogUpProperties {
