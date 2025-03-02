f6a301aa0470de869ee9b808696523caff2d2383
Xasquatch <bigfoot7774@gmail.com>
2025-03-02 21:02:55 +0900

    prop set, get 만 하도록 수정

--- src/main/java/com/forestfull/log/up/spring/LogUpProperties.java
+++ src/main/java/com/forestfull/log/up/spring/LogUpProperties.java
@@ -3,11 +3,14 @@ package com.forestfull.log.up.spring;
 import com.forestfull.log.up.Level;
 import com.forestfull.log.up.util.FileRecorder;
 import com.forestfull.log.up.util.LogFormatter;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Data
+@Setter
+@Getter
 @Component
 @ConfigurationProperties(prefix = "logup")
 public class LogUpProperties {
